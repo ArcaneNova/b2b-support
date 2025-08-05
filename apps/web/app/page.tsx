dde6d4f381b82d3b9b457a1aa87b2e5df33d97b2
officialarshadnoor <blogginghindi@gmail.com>
2025-08-06 02:46:37 +0530

    setup convex

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,10 @@
-import { Button } from "@workspace/ui/components/button"
+"use client"
+import { useQuery } from "convex/react"
+import { api } from "@workspace/backend/_generated/api"
 
 export default function Page() {
+  const
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-      </div>
-    </div>
+    <div className="flex "
   )
 }
